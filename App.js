4da477d18d87202b0d1912fa773f31a2729925fc
Bruno Ferrari <b.ferrari09@gmail.com>
2017-10-11 15:52:51 -0300

    initial commit

---
+++ App.js
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {} from 'react-native';
+import Header from './src/components/header';
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <Header headerText={'Albums'} />
+    );
+  }
+}
