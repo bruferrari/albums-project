fac92ea9b9a490c351299a03d554914321644a4a
Bruno Ferrari <b.ferrari09@gmail.com>
2017-10-11 16:40:08 -0300

    http requests

--- App.js
+++ App.js
@@ -5,13 +5,17 @@
  */
 
 import React, { Component } from 'react';
-import {} from 'react-native';
+import { View } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/albumList';
 
 export default class App extends Component<{}> {
   render() {
     return (
-      <Header headerText={'Albums'} />
+      <View>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+      </View>
     );
   }
 }
---
+++ src/components/albumList.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import Axios from 'axios';
+
+class AlbumList extends Component {
+    state = { albums: [] };
+
+    componentWillMount() {
+        Axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState({ albums: response.data }));
+    }
+
+    render() {
+        console.log(this.state);
+
+        return (
+            <View>
+                <Text>Album List</Text>
+            </View>
+        );
+    }
+}
+
+
+export default AlbumList;
+
